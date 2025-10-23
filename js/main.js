e94d3cfb092692e8e83d4c022d14592a44afc96b
Mathilde <mathilde.asly@gmail.com>
2025-10-23 14:56:19 +0200

    rebuilding the second section and delete the third

--- js/main.js
+++ js/main.js
@@ -4,20 +4,6 @@ gsap.registerPlugin(ScrollTrigger, Observer, ScrollToPlugin, Draggable, MotionPa
 
 /** GSAP Animation - Section 1 */
 
-/** Animations Feuilles */
-
-gsap.from('#feuille1',{
-    x:-100,
-    rotation:-90,
-    duration:3,
-});
-
-gsap.from('#feuille2',{
-    x:100,
-    rotation:90,
-    duration:3,
-})
-
 /** Animation Carroussel */
 
 let tlCarroussel = gsap.timeline({
@@ -31,9 +17,15 @@ tlCarroussel.to('.carousselSection1',{x:'-205vw',duration:20,})
 /** Animations Sardines */
 
 let tlSardines1 = gsap.timeline({
-
-    repeat :-1,
-    yoyo:true
+    scrollTrigger:{
+        trigger:'#section3',
+        start:'top 100%',
+        end:'top 20%',
+        scrub:1,
+        //markers:true,
+        id:"Sardine",
+        toggleActions: 'play none reverse reset',
+    },
 
 });
 
@@ -43,15 +35,42 @@ tlSardines1.to('.sardine1', {y:900, x:1380, stagger : 0.1, duration: 5})
 .to ('sardine1', {y :100, stagger : 0.1})
 
 let tlSardines2 = gsap.timeline({
-
-    repeat: -1,
-    yoyo:true
+    scrollTrigger:{
+        trigger:'#section3',
+        start:'top 100%',
+        end:'top 20%',
+        scrub:1,
+        markers:true,
+        id:"Sardine",
+        toggleActions: 'play none reverse reset',
+    },
+    
 })
 
 tlSardines2.to('.sardine2', {y:-900, x:-1380, stagger: 0.1, duration:5})
 .to ('sardine2', {x:100, stagger:0.1})
 .to ('sardine2', {y :500, stagger : 0.1})
 .to ('sardine2', {y :100, stagger : 0.1,})
+
+/** Scroll Trigger Img Savane */
+
+
+gsap.to("#imgSavane",{
+    scrollTrigger:{
+        trigger:'section3',
+        start:'top 60%',
+        end:'top 20%',
+        scrub:1,
+        //markers:true,
+        id:"imgSavane",
+        toggleActions: 'play none reverse reset',
+    },
+
+    duration: 2,
+})
+
+
+
 
 /**  Animation Bambou */
 
@@ -72,3 +91,28 @@ if (this.hitTest)("carreBambou","40px"){
 } 
 
 */
+
+/** MatchMedia */
+
+let mm = gsap.matchMedia();
+
+mm.add({
+    isMobile: "(max-width:799px)",
+    isDekstop: "(min-width:800px)",
+},(context) =>{
+    let { isMobile, isDekstop } = context.conditions;
+
+    gsap.to("#feuille1", {
+        x:isMobile ? 0 : -100,
+        rotation: isMobile ? 0 : 90,
+        duration:3,
+    })
+
+    gsap.from('#feuille2',{
+    x:isMobile ? 0 : 100,
+    rotation: isMobile ? 0 : 90,
+    duration:3,
+    })
+}
+
+)
